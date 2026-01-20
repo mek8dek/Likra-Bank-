54114340aed4894497d54cafa6a27ec5f392e5fe
mek8dek <cardosoc6679@gmail.com>
2026-01-19 22:05:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let saldo = 100;
 
+function atualizarSaldo() {
+  document.getElementById("saldo").innerText = saldo + " Likra K$";
+}
+
 function ganhar() {
   saldo = saldo + 10;
-  document.getElementById("saldo").innerText = saldo + " Likra K$";
+  atualizarSaldo();
+}
+
+function gastar() {
+  if (saldo >= 5) {
+    saldo = saldo - 5;
+    atualizarSaldo();
+  } else {
+    alert("Saldo insuficiente");
+  }
 }
